12815dab61a50329803a5d1bb686133a5d179247
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2026-01-20 22:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-//your JS code here. If required.
 let button = document.getElementById("btn");
 let ages = document.getElementById("age");
 let names = document.getElementById("name");
+
 button.addEventListener("click", (event) => {
-event.preventDefault();
- let agvalue = ages.value;
- let navalue = names.value;
-if(agvalue == "" && navalue == ""){
-alert("Please enter valid details"); 
-}
+  event.preventDefault();
 
-let P = new Promise((resolve,reject) =>{
- setTimeout(() =>{
-  if(agvalue>=18){
-    resolve("Welcome," + navalue + ".You can vote.");
+  let agvalue = ages.value;
+  let navalue = names.value;
+
+  if (agvalue === "" || navalue === "") {
+    alert("Please enter valid details");
+    return;
   }
-  else{
-    reject("Oh sorry" + navalue + ".You aren't old enough.");
-  }
- }, 4000);
 
+  let P = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (agvalue >= 18) {
+        resolve("Welcome, " + navalue + ". You can vote.");
+      } else {
+        reject("Oh sorry " + navalue + ". You aren't old enough.");
+      }
+    }, 4000);
+  });
 
+  P.then((r) => {
+    alert(r);
+  }).catch((e) => {
+    alert(e);
+  });
 });
-
-P.then((r) => {
- alert(r);
-
-}).catch((e) =>{
-
-alert(e);
-});
-
-})
